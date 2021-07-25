743793eb61491ce16532ec6147c6868a697ec40e
Tianyun Zhang <doowzs@qq.com>
2021-07-25 14:19:01 +0800

    remove extra method in userRepository

--- Server/src/main/java/cn/edu/nju/sicp/repositories/UserRepository.java
+++ Server/src/main/java/cn/edu/nju/sicp/repositories/UserRepository.java
@@ -5,12 +5,8 @@ import cn.edu.nju.sicp.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
-
 @RepositoryRestResource(excerptProjection = UserInfo.class)
 public interface UserRepository extends MongoRepository<User, String> {
-
-    List<User> findAllOrderByUsername();
 
     User findByUsername(String username);
 
