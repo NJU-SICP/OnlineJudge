91d135159fbf62aa9c76940112313fcdc4c9cae6
Tianyun Zhang <doowzs@qq.com>
2021-09-01 14:18:01 +0800

    remove unused beans

--- Server/src/main/java/cn/edu/nju/sicp/configs/BeansConfig.java
+++ Server/src/main/java/cn/edu/nju/sicp/configs/BeansConfig.java
@@ -1,20 +1,13 @@
 package cn.edu.nju.sicp.configs;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
-import org.springframework.core.task.SyncTaskExecutor;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.mapping.RepositoryDetectionStrategy;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.PriorityBlockingQueue;
 
 @Configuration
 public class BeansConfig {
@@ -36,36 +29,6 @@ public class BeansConfig {
     @Bean
     public ProjectionFactory projectionFactory() {
         return new SpelAwareProxyProjectionFactory();
-    }
-
-    @Bean
-    public SimpleAsyncTaskExecutor simpleAsyncTaskExecutor() {
-        return new SimpleAsyncTaskExecutor();
-    }
-
-    @Bean
-    public SyncTaskExecutor syncTaskExecutor() {
-        return new SyncTaskExecutor();
-    }
-
-    @Bean
-    public ThreadPoolTaskExecutor threadPoolTaskExecutor(
-            @Value("${spring.async.core-pool-size}") int corePoolSize,
-            @Value("${spring.async.max-pool-size}") int maxPoolSize,
-            @Value("${spring.async.queue-capacity}") int queueCapacity
-    ) {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor() {
-
-            @Override
-            protected BlockingQueue<Runnable> createQueue(int queueCapacity) {
-                return new PriorityBlockingQueue<>(queueCapacity);
-            }
-
-        };
-        executor.setCorePoolSize(corePoolSize);
-        executor.setMaxPoolSize(maxPoolSize);
-        executor.setQueueCapacity(queueCapacity);
-        return executor;
     }
 
 }
