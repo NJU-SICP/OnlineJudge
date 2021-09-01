bdaf7f84e1d158c78fd56ee936b21f0a4b79b06c
Tianyun Zhang <doowzs@qq.com>
2021-09-01 16:41:15 +0800

    bump version 1.3.0

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,13 +1,13 @@
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
 const config = {
-    version: "v1.2.0",
+    version: "v1.3.0",
     storageKeys: {
         auth: "sicp-auth"
     },
     baseNames: {
         web: isDevelopment ? "" : "/oj",
-        api: isDevelopment ? "http://localhost:8080" : "https://sicp-api.njujb.com"
+        api: isDevelopment ? "https://sicp-api.njujb.com" : "https://sicp-api.njujb.com"
     }
 };
 
