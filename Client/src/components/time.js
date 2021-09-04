41c8fbd6bc9a64ae4388ec4d6efbcc0b99803dee
Tianyun Zhang <doowzs@qq.com>
2021-09-04 16:28:14 +0800

    fix update state after unmounting component

--- Client/src/components/time.js
+++ Client/src/components/time.js
@@ -9,30 +9,36 @@ const Time = () => {
     const [time, setTime] = useState(null);
     const [error, setError] = useState(null);
 
-    const sync = () => {
+    const sync = (dict) => {
         http()
             .get(`/misc/time`)
             .then((res) => {
-                setTime(moment(res.data));
+                if (dict.mounted) {
+                    setTime(moment(res.data));
+                }
             })
             .catch((err) => {
                 console.error(err);
                 message.error("无法连接至服务器，请检查网络环境。");
-                if (!err.response) {
+                if (!err.response && dict.mounted) {
                     setError("无法连接至服务器");
                 }
             });
     };
 
-    const tick = () => {
-        setTime(t => t == null ? null : moment(t).add(1, "seconds"));
+    const tick = (dict) => {
+        if (dict.mounted) {
+            setTime(t => t == null ? null : moment(t).add(1, "seconds"));
+        }
     };
 
     useEffect(() => {
-        sync();
-        const interval1 = setInterval(sync, 60 * 1000);
-        const interval2 = setInterval(tick, 1000);
+        const dict = {mounted: true}; // avoid setState after unmount
+        sync(dict);
+        const interval1 = setInterval(() => sync(dict), 60 * 1000);
+        const interval2 = setInterval(() => tick(dict), 1000);
         return () => {
+            dict.mounted = false;
             clearInterval(interval1);
             clearInterval(interval2);
         };
