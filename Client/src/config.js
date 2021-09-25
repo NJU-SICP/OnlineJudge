0135b5e53c5b2bac8b6743e44d1e8752c1e72296
Tianyun Zhang <doowzs@qq.com>
2021-09-25 13:04:14 +0800

    update api address

--- Client/src/config.js
+++ Client/src/config.js
@@ -7,7 +7,7 @@ const config = {
     },
     baseNames: {
         web: isDevelopment ? "/" : "/oj",
-        api: isDevelopment ? "http://localhost:8080" : "https://sicp-api.njujb.com"
+        api: isDevelopment ? "http://localhost:8080" : "https://sicp-api.njujb.com:28300"
     }
 };
 
