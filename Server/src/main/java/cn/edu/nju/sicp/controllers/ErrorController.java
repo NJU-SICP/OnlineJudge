dbdacee233090b7adfd52a8aa5f545156d178a83
Tianyun Zhang <doowzs@qq.com>
2021-12-09 01:22:37 +0800

    handle invalid http message with 400

--- Server/src/main/java/cn/edu/nju/sicp/controllers/ErrorController.java
+++ Server/src/main/java/cn/edu/nju/sicp/controllers/ErrorController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -33,6 +34,10 @@ public class ErrorController {
                 response.setMessage(rse.getReason());
             } else if (e instanceof AccessDeniedException) {
                 status = HttpStatus.FORBIDDEN;
+                response.setStatus(status.value());
+                response.setMessage(null);
+            } else if (e instanceof HttpMessageNotReadableException) {
+                status = HttpStatus.BAD_REQUEST;
                 response.setStatus(status.value());
                 response.setMessage(null);
             } else if (e instanceof HttpRequestMethodNotSupportedException) {
