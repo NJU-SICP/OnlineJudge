da5008dc67e361096d303713999e147d120748a9
Tianyun Zhang <doowzs@qq.com>
2021-12-10 18:45:29 +0800

    reformat score-table page

--- Client/src/components/admin/score/table.js
+++ Client/src/components/admin/score/table.js
@@ -1,36 +1,22 @@
 import React, {useEffect, useState} from "react";
 import http from "../../../http";
 
-import {Button, Skeleton, Table, Typography} from "antd";
+import {Skeleton, Table, Typography} from "antd";
 import {TableOutlined} from "@ant-design/icons";
 import AdminAssignmentInfo from "../assignments/info";
 import AdminScoreSingle from "./single";
 import {Link} from "react-router-dom";
 
 const AdminScoreTable = () => {
-    const [show, setShow] = useState(false);
+    const [error, setError] = useState(null);
     const [users, setUsers] = useState(null);
-    const [statistics, setStatistics] = useState(null);
     const [columns, setColumns] = useState(null);
 
     useEffect(() => {
-        if (!show) return;
-        http()
-            .get(`/scores/all`)
-            .then((res) => setStatistics(res.data))
-            .catch((err) => console.error(err));
-    }, [show]);
-
-    useEffect(() => {
-        if (!show) return;
-        if (!statistics) return;
-        http()
-            .get(`/users/all`)
-            .then((res) => setUsers(res.data.filter(u => u.roles.indexOf("ROLE_STUDENT") >= 0)))
-            .catch((err) => console.error(err));
-        http()
-            .get(`/assignments/all`)
-            .then((res) => {
+        Promise.all([http().get(`/scores/all`), http().get(`/users/all`), http().get(`/assignments/all`)])
+            .then(([res1, res2, res3]) => {
+                const scores = res1.data;
+                setUsers(res2.data.filter(u => u.roles.indexOf("ROLE_STUDENT") >= 0));
                 const cols = [
                     {
                         title: "学号",
@@ -44,8 +30,8 @@ const AdminScoreTable = () => {
                         key: "fullName"
                     }
                 ];
-                for (const assignment of res.data.reverse()) {
-                    const userStats = statistics[assignment.id] ?? {};
+                for (const assignment of res3.data.reverse()) {
+                    const userStats = scores[assignment.id] ?? {};
                     cols.push({
                         title: <>
                             {assignment.slug
@@ -59,22 +45,22 @@ const AdminScoreTable = () => {
                 }
                 setColumns(cols);
             })
-            .catch((err) => console.error(err));
-    }, [show, statistics]);
+            .catch((err) => {
+                console.error(err);
+                setError(err);
+            });
+    }, []);
 
     return (<>
         <Typography.Title level={2}>
             <TableOutlined /> 成绩查询
         </Typography.Title>
-        {!show
-            ? <div>
-                <p>本页面用于展示所有学生的作业和成绩，由于技术过烂，服务器计算所有数据需要大约1分钟时间，请务必耐心等待。如需查看，请点击下方按钮。</p>
-                <p><Button onClick={() => setShow(true)}>查看所有成绩</Button></p>
-            </div>
+        {!columns && !error
+            ? <Skeleton />
             : <>
-                {!statistics || !users || !columns
-                    ? <Skeleton />
-                    : <Table columns={columns} dataSource={users} rowKey="id" pagination={false} />
+                {!error
+                    ? <Table columns={columns} dataSource={users} rowKey="id" pagination={false} />
+                    : <Typography.Text>加载失败：{error}</Typography.Text>
                 }
             </>
         }
