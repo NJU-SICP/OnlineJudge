afe07917cfb6bb2f6a04f400a4154b5ccfdaa107
Tianyun Zhang <doowzs@qq.com>
2021-12-12 01:42:36 +0800

    bump version 2.0.1

--- Client/src/config.js
+++ Client/src/config.js
@@ -1,7 +1,7 @@
 const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
 const config = {
-    version: "v2.0.0",
+    version: "v2.0.1",
     storageKeys: {
         auth: "sicp-auth"
     },
