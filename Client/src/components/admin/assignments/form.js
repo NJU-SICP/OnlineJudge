f53f4954455332727fc979b02ff530b4f10ace6b
Tianyun Zhang <doowzs@qq.com>
2021-12-12 00:02:18 +0800

    add *.sql as a valid submit file type

--- Client/src/components/admin/assignments/form.js
+++ Client/src/components/admin/assignments/form.js
@@ -28,6 +28,7 @@ const AdminAssignmentForm = ({initialValues, onFinish, disabled}) => {
                     <Radio value=".pdf">PDF文件（*.pdf）</Radio>
                     <Radio value=".py">Python代码文件（*.py）</Radio>
                     <Radio value=".scm">Scheme代码文件（*.scm）</Radio>
+                    <Radio value=".sql">SQL代码文件（*.sql）</Radio>
                     <Radio value=".zip">压缩文件夹（*.zip）</Radio>
                 </Radio.Group>
             </Form.Item>
